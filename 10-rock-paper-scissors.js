0051b28e01a50953b7f977f3d79355e04aae0059
AhmadBakri7 <140534294+AhmadBakri7@users.noreply.github.com>
2023-11-25 20:07:11 +0200

    Update 10-rock-paper-scissors.js

--- 10-rock-paper-scissors.js
+++ 10-rock-paper-scissors.js
@@ -6,6 +6,44 @@ let score = JSON.parse(localStorage.getItem("score")) || {
 
 updateScoreElement();
 
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+  if (!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  }
+}
+
+document.querySelector(".js-rock-button").addEventListener("click", () => {
+  playGame("rock");
+});
+
+document.querySelector(".js-paper-button").addEventListener("click", () => {
+  playGame("paper");
+});
+
+document.querySelector(".js-scissors-button").addEventListener("click", () => {
+  playGame("scissors");
+});
+
+document.body.addEventListener("keydown", (event) => {
+  if (event.key === "r") {
+    playGame("rock");
+  } else if (event.key === "p") {
+    playGame("paper");
+  } else if (event.key === "s") {
+    playGame("scissors");
+  }
+});
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
 
@@ -51,7 +89,7 @@ function playGame(playerMove) {
 
   document.querySelector(".js-result").innerHTML = result;
 
-  document.querySelector(".js-moves").innerHTML = ` You
+  document.querySelector(".js-moves").innerHTML = `You
 <img src="images/${playerMove}-emoji.png" class="move-icon">
 <img src="images/${computerMove}-emoji.png" class="move-icon">
 Computer`;
